1f3efb75b6373dc4efd33c1dac844f59b4bb2da6
Islom_the_dev <polzovatelit1@gmail.com>
2024-08-15 13:27:54 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let question = +prompt("Введите текущее время!")
+
+switch (question) {
+  case 6:
+  case 7:
+  case 8:
+  case 9:
+  case 10:
+  case 11:
+  case 12:
+    alert("Доброе утро Вавилон!!!")
+    console.log("Доброе утро Вавилон!!!");
+    
+    break;
+  case 12:
+  case 13:
+  case 14:
+  case 15:
+  case 16:
+  case 17:
+  case 18:
+    alert("*Звук включения Windows XP*")
+    console.log("*Звук включения Windows XP*");
+    break;
+  case 18:
+  case 19:
+  case 20:
+  case 21:
+  case 22:
+  case 23:
+  case 24:
+    alert("Добрый вечер !!!")
+    console.log("Добрый вечер !!!");
+    break;
+
+  default:
+    alert("*Звук выключения Windows XP*")
+    console.log("*Звук выключения Windows XP*");
+    break;
+}
+
+
